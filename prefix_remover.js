44448a95d869c3f55bdd76acc94230001724c9f3
Anton Bakun <anton.bakun.s@gmail.com>
2017-10-18 12:31:39 +0300

    fixed

--- prefix_remover.js
+++ prefix_remover.js
@@ -1,30 +1,35 @@
 var fs = require('fs');
+var xelement = require('xelement')
 
-if (process.argv.length <= 3) {
+if (process.argv.length <= 2) {
     console.log("Usage: " + __filename + " SOME_PARAM");
     process.exit(-1);
 }
  
-var path = process.argv[2]; / path 
-var prefix = process.argv[3]; /prefix name 
+var path = process.argv[2]; // path 
+var prefix = '';//prefix name 
 
 console.log('Path: ' + path);
 console.log('Prefix: ' + prefix);
 
-var dirnames =['classes', 'triggers', 'pages', 'components', 'labels'];/Array of Dirnames
+var xml = fs.readFileSync(path + '/package.xml', 'utf8');
+xeleCatalog = xelement.Parse(xml);
+prefix = xeleCatalog.getElementValue('namespacePrefix');
+if(prefix == null || prefix == undefined){
+   process.exit(-1);
+}
 
-dirnames.forEach(function(item, index, array) { / loop for dirnames 
+
+var dirnames =['classes', 'triggers', 'pages', 'components'];//Array of Dirnames add labels in future
+
+dirnames.forEach(function(item, index, array) { // loop for dirnames 
     var pathFor = path + '/' + item;
-    fs.readdir(pathFor, function(err, items) { / read all files in dir 
-        if (items.length > 1) {
-            for (var i=0; i<items.length; i++) { / read file and delete prefix 
-                console.log(pathFor + '/' + items[i]);
-                var text = fs.readFileSync(pathFor + '/' + items[i], 'utf8'); / delet prefix 
-                text = text.replace(new RegExp(prefix + '__|' + prefix + '.', 'gi'), '');
-                fs.writeFile(pathFor + '/' + items[i], text); / write file 
-            }
-        } else {
-            break(); / if folder empty;
+    fs.readdir(pathFor, function(err, items) { // read all files in dir 
+        for (var i=0; i<items.length; i++) { // read file and delete prefix 
+            console.log(pathFor + '/' + items[i]);
+            var text = fs.readFileSync(pathFor + '/' + items[i], 'utf8'); // delet prefix 
+            text = text.replace(new RegExp(prefix + '__|' + prefix + '.', 'gi'), '');
+            fs.writeFile(pathFor + '/' + items[i], text); // write file 
         }
     });
 });
